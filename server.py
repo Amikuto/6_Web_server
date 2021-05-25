a5be35935d1ea1bf1c1eb02a46a884cd4b0bd8ad
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-05-25 19:14:51 +0300

    Some changes

--- server.py
+++ server.py
@@ -1,31 +1,110 @@
 import socket
+import yaml
+import os
+import utils
 
-sock = socket.socket()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+def read_config() -> dict:
+    """Чтение настроек из файла yaml"""
+    with open("settings.yaml", "r") as file:
+        return yaml.safe_load(file)
 
-sock.listen(5)
 
-conn, addr = sock.accept()
-print("Connected", addr)
+class Socket:
+    def __init__(self, host, port, buffer):
+        self.host = host
+        self.port = port
+        self.buffer = buffer
 
-data = conn.recv(8192)
-msg = data.decode()
+    def __repr__(self):
+        return f"<ServerSocket {self.host}:{self.port}>"
 
-print(msg)
+    def start(self):
+        assert self._socket is None, "ServerSocket уже открыт"
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            self._socket.bind((self.host, self.port))
+        except Exception:
+            self.close()
+            raise
+        else:
+            self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
+    def close(self):
+        assert self._socket is not None, "Данный ServerSocket уже был закрыт"
+        if self._connection:
+            self._connection.close()
+            self._connection = None
+        self._socket.close()
+        self._socket = None
 
-Hello, webworld!"""
 
-conn.send(resp.encode())
+class Server:
+    def __init__(self):
+        self.socket = socket.socket()
+        self.homedir = os.path.abspath(read_config()["HOMEDIR"])
 
-conn.close()
+    def open(self):
+        self.socket.bind(('localhost', 80))
+        self.socket.listen(5)
+
+    def accept_client(self):
+        cli_sock, cli_add = self.socket.accept()
+
+
+def main():
+    server = Server()
+    server.open()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sock = socket.socket()
+#
+# try:
+#     sock.bind(('', 80))
+#     print("Using port 80")
+# except OSError:
+#     sock.bind(('', 8080))
+#     print("Using port 8080")
+#
+# sock.listen(5)
+#
+# conn, addr = sock.accept()
+# print("Connected", addr)
+#
+# data = conn.recv(8192)
+# msg = data.decode()
+#
+# print(msg)
+#
+# resp = """HTTP/1.1 200 OK
+# Server: SelfMadeServer v0.0.1
+# Content-type: text/html
+# Connection: close
+#
+# Hello, webworld!"""
+#
+# conn.send(resp.encode())
+#
+# conn.close()
---
+++ test_server.py
@@ -0,0 +1,85 @@
+import logging
+import os
+import socket
+import utils
+from datetime import datetime
+from pprint import pprint
+
+
+class Request:
+    def __init__(self, data: bytes):
+        self.text = []
+        for i in data.decode("utf8", "replace").split("\n"):
+            string = i.strip()
+            if string:
+                self.text.append(string)
+        # print(text)
+
+    def getSmth(self):
+        return self.text
+
+
+class Socket:
+    def __init__(self, address: str, port: int):
+        self.sock = socket.socket()
+        self.address = address
+        self.port = port
+        self.is_open = False
+
+    def __repr__(self):
+        return f"<ServerSocket {self.address}:{self.port}>"
+
+    def open(self):
+        self.sock.bind((self.address, self.port))
+        self.is_open = True
+
+    def close(self):
+        self.sock.close()
+
+    def accept(self):
+        self.sock.listen(5)
+        self._connection, self._user_address = self.sock.accept()
+        # data = self._connection.recv(512)
+        # return Request(data).getSmth()
+
+    def send(self, data: bytes):
+        self._connection.send(data)
+
+
+class Server:
+    def __init__(self, address="localhost", host=80):
+        self.socket = Socket(address, host)
+
+    def start(self):
+        self.socket.open()
+        while True:
+            self.client_request()
+
+    def stop(self):
+        self.socket.close()
+
+    def client_request(self):
+        pprint(self.socket.accept())
+        date = datetime.now()  # "Wed, 21 Oct 2015 07:28:00 GMT"
+        content_length = 8000
+        content_type = "text/html"
+        server_html = "close"
+
+        response = "HTTP/1.1 200 OK\n" \
+                   "Content-Type: {}\n" \
+                   "Date: {}\n" \
+                   "Content-length: {}\n" \
+                   "Server: {}\n" \
+                   "{}"
+
+        body = utils.read_file("index")
+        r = response.format(content_type, date, content_length, server_html, body)
+        self.socket.send(r.encode())
+
+
+if __name__ == "__main__":
+    settings = utils.read_config()
+    HOST, PORT = settings["ADDRESS"], settings["PORT"]
+
+    server = Server(HOST, PORT)
+    server.start()
