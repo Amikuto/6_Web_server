9bc6ef852a32d6e6bb1f84b0441957a0a787a82e
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-06-01 13:21:26 +0300

    6-web

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def read_file(filename):
-    with open(f"static/html/{filename}.html", 'r', encoding='UTF-8') as file:
+    with open(f"static/html/{filename}", 'r', encoding='UTF-8') as file:
         return file.read()
 
 
